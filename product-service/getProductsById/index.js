aec0d014bd4d15cabfe5f1803e08b91c04d32a59
cilga <cilgaiscan@gmail.com>
2023-03-06 22:41:13 +0100

    Connect /products/{productId} endpoint to DB

--- product-service/getProductsById/index.js
+++ product-service/getProductsById/index.js
@@ -3,19 +3,28 @@ const { ProductService } = require('../product.service');
 module.exports.handler = async (event) => {
   const { productId } = event.pathParameters;
 
-  const product = await ProductService.getById(productId);
-
-  if (product) {
+  try {
+    const product = await ProductService.getById(productId);
     return {
       statusCode: 200,
       body: JSON.stringify(product),
-    };
-  } else {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({
-        message: "NOT FOUND!",
-      }),
+    }
+  } catch (err) {
+    console.error(err);
+    if (err.message.toLowerCase().includes('not found')) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "NOT FOUND!",
+        }),
+      }
+    } else {
+      return {
+        statusCode: 418,
+        body: JSON.stringify({
+          message: "Unknown Error!",
+        }),
+      }
     }
   }
 };
--- product-service/product.repo.js
+++ product-service/product.repo.js
@@ -6,22 +6,56 @@ class ProductRepo {
   }
 
   async _scanTable(tableName) {
-    return await this.awsClient.scan({
+    return await this.awsClient
+    .scan({
       TableName: tableName,
-    }).promise().then((data) => data.Items);
+    })
+    .promise()
+    .then((data) => data.Items);
+  }
+
+  async _getItem(tableName, filter) {
+    return await this.awsClient
+    .get({
+      TableName: tableName,
+      Key: filter
+    })
+    .promise()
+    .then((data) => data.Item);
   }
 
   async scanAllWithStocks() {
     const products = await this._scanTable(process.env.PRODUCTS_TABLE_NAME);
     const stocks = await this._scanTable(process.env.STOCKS_TABLE_NAME);
-    
+
     return products.map((p) => {
       const stock = stocks.find((s) => s.product_id === p.id);
       return {
-        ...p, 
+        ...p,
         count: stock.count,
       };
     })
+  }
+
+  async getById(id) {
+    const product = await this._getItem(process.env.PRODUCTS_TABLE_NAME, {
+      id: id,
+    });
+    if (!product) {
+      throw new Exception(`Product not found! Product Id: ${id}`);
+    }
+
+    const stock = await this._getItem(process.env.STOCKS_TABLE_NAME, {
+      product_id: id,
+    });
+    if (!stock) {
+      throw new Exception(`Stock not found for the product! Product Id: ${id}`);
+    }
+
+    return {
+      ...product,
+      count: stock.count,
+    }
   }
 }
 
