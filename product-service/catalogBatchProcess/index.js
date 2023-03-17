07ca9b96ea9b16f4b0b7732cf4e57835fef1b351
cilga <cilgaiscan@gmail.com>
2023-03-17 21:57:12 +0100

    Add product creation step for catalogBatchProcess

--- product-service/catalogBatchProcess/index.js
+++ product-service/catalogBatchProcess/index.js
@@ -1,12 +1,28 @@
-function processProduct(record) {
+const { ProductService } = require('../product.service');
+
+
+async function processProduct(record) {
   const productData = typeof record.body == 'string' ? JSON.parse(record.body) : record.body;
-  console.log(JSON.stringify(productData));
+  const {title, description, count, price} = productData;
+
+  try {
+    const product = await ProductService.create({
+      title,
+      description,
+      price: Number.parseFloat(price),
+      count: Number.parseFloat(count),
+    });
+  
+    console.debug(`Product (#${product.id}) Processed!`);
+  } catch (e) {
+    console.error('Error occurred while creating the product!', e);
+  }  
 }
 
 module.exports.handler = async (event) => {
-  const {Records: records} = event;
-
-  records.forEach(processProduct);
+  for (const record of event.Records) {
+    await processProduct(record);
+  }
 
   return {
     statusCode: 200,
