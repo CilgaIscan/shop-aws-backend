646562ce63f89ec7c8216e40449e051053221ae6
cilga <cilgaiscan@gmail.com>
2023-03-17 22:39:30 +0100

    Add email notification step to catalogBatchProcess

--- product-service/catalogBatchProcess/index.js
+++ product-service/catalogBatchProcess/index.js
@@ -1,5 +1,7 @@
 const { ProductService } = require('../product.service');
+const SNS = require('../aws/sns.service');
 
+SNS_TOPIC_ARN = process.env.SNS_TOPIC_ARN;
 
 async function processProduct(record) {
   const productData = typeof record.body == 'string' ? JSON.parse(record.body) : record.body;
@@ -16,13 +18,25 @@ async function processProduct(record) {
     console.debug(`Product (#${product.id}) Processed!`);
   } catch (e) {
     console.error('Error occurred while creating the product!', e);
-  }  
+  }
+}
+
+async function notifyAll() {
+  try {
+    await SNS.publish('Batch products creation finished!', SNS_TOPIC_ARN);
+
+    console.debug(`Notification sent to ${SNS_TOPIC_ARN}!`);
+  } catch (e) {
+    console.error(`Error occurred while sending the notification to ${SNS_TOPIC_ARN}!`, e);
+  }
 }
 
 module.exports.handler = async (event) => {
   for (const record of event.Records) {
     await processProduct(record);
   }
+
+  await notifyAll();
 
   return {
     statusCode: 200,
